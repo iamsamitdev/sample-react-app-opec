d3b1eeeb9919babbc8027b5b77c51128d5c1ca85
Samit Koyom <samitkoyom@gmail.com>
2025-06-11 15:44:03 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { AppRouter } from "./components/routes"
+
+function App() {
+  return (
+    <AppRouter />
+  )
+}
+export default App
---
+++ src/components/routes/index.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router"
+import MainLayout from "@/layouts/MainLayout"
+import Home from "@/pages/Home"
+import About from "@/pages/About"
+import Service from "@/pages/Service"
+import Contact from "@/pages/Contact"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: "about",
+                element: <About />
+            },
+            {
+                path: "service",
+                element: <Service />
+            },
+            {
+                path: "contact",
+                element: <Contact />
+            }
+        ]
+    }
+])
+
+export const AppRouter = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
